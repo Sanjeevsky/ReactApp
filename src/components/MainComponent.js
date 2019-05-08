772090014481e8332d07ee311e208e321d28e73a
sanjeevsky <knit16643@gmail.com>
2019-05-08 20:32:02 +0530

    Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,11 +4,15 @@ import Home from "./HomeComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Contact from "./ContactComponent";
-import { addComment } from "../redux/ActionCreators";
 import DishDetail from "./DishdetailComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { addComment, fetchDishes } from '../redux/ActionCreators';
+
+
+
+
 
 const mapStateToProps = state => {
   return {
@@ -20,7 +24,8 @@ const mapStateToProps = state => {
 };
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) =>
-    dispatch(addComment(dishId, rating, author, comment))
+    dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes())}
 });
 
 class Main extends Component {
@@ -29,36 +34,36 @@ class Main extends Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId });
   }
 
   render() {
-    const DishWithId = ({ match }) => {
-      return (
-        <DishDetail
-          dish={
-            this.props.dishes.filter(
-              dish => dish.id === parseInt(match.params.dishId, 10)
-            )[0]
-          }
-          comments={this.props.comments.filter(
-            comment => comment.dishId === parseInt(match.params.dishId, 10)
-          )}
-          addComment={this.props.addComment}
-        />
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment}
+          />
       );
     };
     //Home Component
-    const HomePage = () => {
-      return (
-        <Home
-          dish={this.props.dishes.filter(dish => dish.featured)[0]}
-          promotion={this.props.promotions.filter(promo => promo.featured)[0]}
-          leader={this.props.leaders.filter(leader => leader.featured)[0]}
-        />
+   const HomePage = () => {
+      return(
+          <Home 
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
-    };
+    }
     const AboutPage = () => {
       return <About leaders={this.props.leaders} />;
     };
