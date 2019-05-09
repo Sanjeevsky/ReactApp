85578ead50f6f78cf55217d6f0f0b1e13cf6b411
sanjeevsky <knit16643@gmail.com>
2019-05-09 21:27:43 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,6 +20,7 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
 const minLength = len => val => val && val.length >= len;
@@ -146,6 +147,7 @@ function RenderComments({comments, postComment, dishId}) {
   if (comments != null) {
     const comment = comments.map(comment => {
       return (
+        <Fade in>
         <li key={comment.id}>
           <p>{comment.comment}</p>
           <p>
@@ -157,13 +159,16 @@ function RenderComments({comments, postComment, dishId}) {
             }).format(new Date(comment.date))}
           </p>
         </li>
-       
+       </Fade>
       );
     });
     return (
       <div className="col-12 col-md-5 m-1">
         <h4> Comments </h4>
+        <Stagger in>
         <ul className="list-unstyled">{comment}</ul>
+        </Stagger>
+        
         <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     );
